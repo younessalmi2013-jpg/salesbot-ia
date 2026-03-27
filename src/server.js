f52f82b51e5d94f589cbdb00d8663e3cd876d25b
younessalmi2013-jpg <younes.salmi2013@gmail.com>
2026-03-27 15:33:25 +0100

    fix: add cookie-parser middleware (auth was broken without it)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const path = require('path');
 const fs = require('fs');
 
@@ -39,6 +40,7 @@ function saveConfig(cfg) {
 // =====================
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use((req, res, next) => {
